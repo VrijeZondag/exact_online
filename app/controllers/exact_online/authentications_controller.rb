c20ede596f220e302ef6035556c0db1d669e25f2
badbusiness <martijn@hovetechniek.nl>
2023-12-26 15:17:35 +0100

    Update authentications_controller.rb

--- app/controllers/exact_online/authentications_controller.rb
+++ app/controllers/exact_online/authentications_controller.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-module Public
-  module ExactOnline
-    class AuthenticationsController < Base
-      def new
-        redirect_to ::ExactOnline::Client.new.authorize_url, allow_other_host: true
-      end
+module ExactOnline
+  class AuthenticationsController < Base
+    def new
+      redirect_to ::ExactOnline::Client.new.authorize_url, allow_other_host: true
+    end
 
-      def webhook
-        ::ExactOnline::Client.new.receive_code(params['code'])
+    def webhook
+      ::ExactOnline::Client.new.receive_code(params['code'])
 
-        redirect_to root_path, notice: 'Exact Online is geautoriseerd'
-      end
+      redirect_to root_path, notice: 'Exact Online is geautoriseerd'
     end
   end
 end
